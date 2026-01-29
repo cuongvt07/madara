67ced3be6c65b92de199154fb3f7a635c3828955
cuong pham <cuongdtnt109@gmail.com>
2026-01-29 09:37:27 +0700

    Ignore wp-config.php

--- wp-content/themes/madara-child/functions.php
+++ wp-content/themes/madara-child/functions.php
@@ -27,7 +27,7 @@ function madara_scripts_styles_child_theme()
 
 	wp_enqueue_style('madara-css-child', get_stylesheet_directory_uri() . '/style.css', array(
 		'madara-css'
-	), '1.0.5');
+	), '1.0.7');
 
 	// Enqueue Swiper.js for vertical carousel
 	wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.0');
@@ -106,3 +106,147 @@ add_filter('madara_archive_chapter_date', function ($time_diff, $chapter_id, $da
 	}
 	return $time_diff;
 }, 20, 4);
+
+
+/**
+ * Shortcode: [manga_new_updates]
+ * Thay thế widget Madara New Updates
+ */
+
+add_shortcode('manga_new_updates', function ($atts) {
+
+	if (!class_exists('WP_MANGA')) {
+		return '<p><strong>WP Manga plugin chưa được kích hoạt.</strong></p>';
+	}
+
+	global $wp_manga_template;
+
+	/* ======================
+	 * SHORTCODE ATTRS
+	 * ====================== */
+	$atts = shortcode_atts([
+		'title' => '',
+		'posts_per_page' => 24,
+		'columns' => 6,
+		'style' => 'style-1', // style-1 | style-2
+		'show_badges' => 1,
+		'show_chapter_count' => 1,
+		'show_update_date' => 1,
+		'pagination' => 1,
+	], $atts);
+
+	/* ======================
+	 * PAGINATION
+	 * ====================== */
+	$paged = max(
+		1,
+		get_query_var('paged') ?: get_query_var('page')
+	);
+
+	/* ======================
+	 * QUERY
+	 * ====================== */
+	$q = new WP_Query([
+		'post_type' => 'wp-manga',
+		'post_status' => 'publish',
+		'posts_per_page' => (int) $atts['posts_per_page'],
+		'paged' => $paged,
+		'orderby' => 'modified',
+		'order' => 'DESC',
+	]);
+
+	/* ======================
+	 * COLUMN CLASS
+	 * ====================== */
+	$col = max(1, min(6, (int) $atts['columns']));
+	$col_class = 'col-md-' . floor(12 / $col) . ' col-sm-4 col-xs-6';
+
+	ob_start();
+	?>
+
+	<div class="widget madara-child-new-updates">
+
+		<?php if (!empty($atts['title'])): ?>
+			<h2 class="widget-title heading-style-1">
+				<?php echo esc_html($atts['title']); ?>
+			</h2>
+		<?php endif; ?>
+
+		<!-- GIỮ NGUYÊN HEADING ẢNH CỦA WIDGET -->
+		<div class="img_heading">
+			<img decoding="async" src="https://vitaminkoo.com/wp-content/themes/gdcv/img/MCN.png">
+		</div>
+
+		<?php if ($q->have_posts()): ?>
+
+			<div class="c-blog-listing mad-new-updates-widget-grid">
+				<div class="c-blog-grid">
+					<div class="row">
+
+						<?php
+						while ($q->have_posts()):
+							$q->the_post();
+
+							set_query_var('madara_widget_settings', [
+								'show_badges' => (bool) $atts['show_badges'],
+								'show_chapter_count' => (bool) $atts['show_chapter_count'],
+								'show_update_date' => (bool) $atts['show_update_date'],
+								'col_class' => $col_class,
+								'style' => $atts['style'],
+							]);
+							?>
+
+							<div class="<?php echo esc_attr($col_class); ?> page-listing-item internal-widget">
+								<div class="popular-item-wrap">
+
+									<?php
+									if ($atts['style'] === 'style-1') {
+										$wp_manga_template->load_template(
+											'widgets/recent-manga/content-1',
+											false
+										);
+									} else {
+										$wp_manga_template->load_template(
+											'widgets/recent-manga/content-2',
+											false
+										);
+									}
+									?>
+
+								</div>
+							</div>
+
+						<?php endwhile; ?>
+
+					</div>
+				</div>
+			</div>
+
+			<?php if ($atts['pagination']): ?>
+				<div class="mad-pagination text-center">
+					<?php
+					echo paginate_links([
+						'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+						'format' => '?paged=%#%',
+						'current' => max(1, $paged),
+						'total' => $q->max_num_pages,
+						'mid_size' => 2,
+						'end_size' => 1,
+						'prev_text' => '<i class="icon ion-ios-arrow-back"></i>',
+						'next_text' => '<i class="icon ion-ios-arrow-forward"></i>',
+					]);
+					?>
+				</div>
+			<?php endif; ?>
+
+			<?php wp_reset_postdata(); ?>
+
+		<?php else: ?>
+			<p>Không có manga nào.</p>
+		<?php endif; ?>
+
+	</div>
+
+	<?php
+	return ob_get_clean();
+});
